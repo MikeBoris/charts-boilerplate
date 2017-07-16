232b7eb6880fd07cc791c2c6bdfa908bda8ab1d7
MikeBoris <mikeboris123@gmail.com>
2017-07-16 10:19:06 -0400

    added url routing

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-	return render_template('index.html')
+@app.route("/<name>")
+def main(name):
+	title = name
+	return render_template('index.html', name=name, title=title)
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
