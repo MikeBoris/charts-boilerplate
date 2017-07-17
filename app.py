5acd5e7ce180362d0fea164bb558c0c2509325ec
MikeBoris <mikeboris123@gmail.com>
2017-07-16 23:32:29 -0400

    added test sqlite db, example table

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask import render_template
+import sqlite3
+import pandas as pd
+import numpy as np
+
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/<name>")
-def main(name):
+DATABASE = 'data/TEST_DB.db'
+
+query = """
+select * from fruit;
+"""
+
+@app.route('/<name>')
+def charts(name):
+	cnxn = sqlite3.connect(DATABASE)
+	df = pd.read_sql_query(query, cnxn)
+	data = df.to_json(orient='index')
 	title = name
-	return render_template('index.html', name=name, title=title)
+	return render_template('index.html', name=name, title=title, data=data)
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
